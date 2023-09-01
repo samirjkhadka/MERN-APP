f8cbef66eca802f65dc9ba5465b60f936a42cb52
samirjkhadka <findsamirkhadka@gmail.com>
2023-09-01 16:54:53 +0545

    Username component UI complete

--- Login App/client/src/components/Username.js
+++ Login App/client/src/components/Username.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
+import {Link} from "react-router-dom"
+import avatar  from '../assets/images/profile.png'
+import styles from '../styles/Username.module.css'
 
 export default function username() {
   return (
-    <div>username</div>
-  )
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}> 
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Hello again</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-50">
+              Explore More by connecting with us
+            </span>
+          </div>
+          <form className="py-1">
+            <div className="profile flex justify-center py-4">
+              <img src={avatar} className={styles.profile_img} alt="avatar"></img>
+            </div>
+            <div className="textbox flex flex-col items-center gap-6">
+              <input type="text" className={styles.textbox} placeholder="Username"></input>
+              <button className={styles.btn} type="submit">Let's Go</button>
+            </div>
+            <div className="text-center py-4">
+              <span className="text-gray-500">
+                {" "}
+                Not a member{" "}
+                <Link className="text-red-500" to="/register">
+                  Register
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
